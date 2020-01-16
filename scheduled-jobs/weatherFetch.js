c02b217368d1eafc49f5938f4c05361764bc65d5
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2020-01-15 23:47:51 -0500

    Added db closing logic

--- scheduled-jobs/weatherFetch.js
+++ scheduled-jobs/weatherFetch.js
@@ -7,20 +7,42 @@ let cities = JSON.parse(rawCities);
 const logError = require("../Globals/logError");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true });
 
+let runningSaves = 0;
+//used for testing
+// Weather.create(JSON.parse(fs.readFileSync("./mockData.json"))).then(() => runningSaves--);
 const runFetch = () => {
     for (let i = 0; i < cities.length; i++) {
-
+        runningSaves++;
         axios.get(`https://api.darksky.net/forecast/${process.env.DARKSKY_API}/${cities[i].latitude},${cities[i].longitude}`).then((weatherData) => {
-            Weather.create(weatherData.data);
+            runningSaves++;
+            Weather.create(weatherData.data).then(() => runningSaves--).catch(() => runningSaves--);
         }).catch((err) => {
+            runningSaves--
             logError(err);
             console.error(err);
         });
     };
 }
 
+
+function closeConnection() {
+    console.log(runningSaves);
+    if (runningSaves === 0) {
+        mongoose.disconnect();
+        console.log("Mongoose Disconnected.")
+    } else {
+        console.log("waiting to close");
+        setTimeout(function () {
+            closeConnection(runningSaves);
+        }, 1000)
+    }
+};
+
 runFetch();
+
+closeConnection(runningSaves);
+
 
 // module.exports = runFetch;
