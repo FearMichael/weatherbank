c08333854525c01c5f97b26cfea2373bc38c877e
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2020-01-18 17:45:41 -0500

    Change close connection params

--- scheduled-jobs/weatherFetch.js
+++ scheduled-jobs/weatherFetch.js
@@ -31,7 +31,7 @@ const runFetch = async () => {
 
 function closeConnection() {
     console.log(runningSaves);
-    if (runningSaves === 0) {
+    if (runningSaves <= 0) {
         mongoose.disconnect();
         console.log("Mongoose Disconnected.")
     } else {
