223a5a7b19012465ae9ce8a479b60a59bf817259
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2020-01-22 19:47:16 -0500

    adding mongo error handling

--- scheduled-jobs/weatherFetch.js
+++ scheduled-jobs/weatherFetch.js
@@ -6,61 +6,65 @@ const cities = JSON.parse(fs.readFileSync("./cities.json"));
 const logError = require("../Globals/logError");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then(function () {
 
-//Kill process after 7 minutes
-const maxRuntime = setTimeout(function () {
-    console.error("Runtime Maxed Out - Killing process")
-    mongoose.disconnect()
-    process.exit(1);
-    //7 minute timeout
-}, (1000 * 60) * 7);
 
-//Count how many items get saved to the database then disconnect from DB and end process once 1000 have been run
-let savedItems = null;
 
-const runFetch = () => {
-    //loop over all cities and fetch their weather data
-    for (let i = 0; i < cities.length; i++) {
-        axios.get(`https://api.darksky.net/forecast/${process.env.DARKSKY_API}/${cities[i].latitude},${cities[i].longitude}`).then((weatherData) => {
-            //add weather data for the city to the DB
-            Weather.create(weatherData.data).then(() => {
-                //increment saved items forward upon successful completion
-                savedItems++
-                checkCloseConnection();
+    //Kill process after 7 minutes
+    const maxRuntime = setTimeout(function () {
+        console.error("Runtime Maxed Out - Killing process")
+        mongoose.disconnect()
+        process.exit(1);
+        //7 minute timeout
+    }, (1000 * 60) * 7);
+
+    //Count how many items get saved to the database then disconnect from DB and end process once 1000 have been run
+    let savedItems = null;
+
+    const runFetch = () => {
+        //loop over all cities and fetch their weather data
+        for (let i = 0; i < cities.length; i++) {
+            axios.get(`https://api.darksky.net/forecast/${process.env.DARKSKY_API}/${cities[i].latitude},${cities[i].longitude}`).then((weatherData) => {
+                //add weather data for the city to the DB
+                Weather.create(weatherData.data).then(() => {
+                    //increment saved items forward upon successful completion
+                    savedItems++
+                    checkCloseConnection();
+                }).catch(err => {
+                    //increment saved items forward even when failed but log the failure
+                    //this way the checkCloseFunction will still end after cities.length has been completed
+                    savedItems++
+                    checkCloseConnection();
+                    console.error(err);
+                })
             }).catch(err => {
-                //increment saved items forward even when failed but log the failure
-                //this way the checkCloseFunction will still end after cities.length has been completed
                 savedItems++
-                checkCloseConnection();
-                console.error(err);
+                if (err.response.code === 403 || err.response.data.code === 403) {
+                    //if forbidden at DarkSky - most likely the API call limit has been reached, disconnect and terminate the process
+                    console.error("Exited on 403 error");
+                    mongoose.disconnect();
+                    process.exit(1);
+                } else {
+                    checkCloseConnection();
+                }
             })
-        }).catch(err => {
-            savedItems++
-            if (err.response.code === 403 || err.response.data.code === 403) {
-                //if forbidden at DarkSky - most likely the API call limit has been reached, disconnect and terminate the process
-                console.error("Exited on 403 error");
-                mongoose.disconnect();
-                process.exit(1);
-            } else {
-                checkCloseConnection();
-            }
-        })
+        };
     };
-};
 
-function checkCloseConnection() {
-    if (savedItems === cities.length) {
-        mongoose.disconnect();
-        console.log("Mongoose Disconnected.");
-        console.log("Successfully Completed");
-        process.exit(0);
+    function checkCloseConnection() {
+        if (savedItems === cities.length) {
+            mongoose.disconnect();
+            console.log("Mongoose Disconnected.");
+            console.log("Successfully Completed");
+            process.exit(0);
+        };
     };
-};
 
-if (process.argv[2] === "run") {
-    runFetch();
-} else {
-    console.error("Add 'run' to your command")
-    process.exit(1);
-}
+    if (process.argv[2] === "run") {
+        runFetch();
+    } else {
+        console.error("Add 'run' to your command")
+        process.exit(1);
+    }
+
+}).catch(err => process.exit(1));
